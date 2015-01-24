c51d433bd783368ec657495c2ed565d793569d9f
Atmatm6 Yolo <atmatm6000@gmail.com>
2015-01-24 09:13:24 -0600

    Readmecommit

--- src/main/java/com/Atmatm6/schematichub/SchematicGithubConnector.java
+++ src/main/java/com/Atmatm6/schematichub/SchematicGithubConnector.java
@@ -1,11 +1,13 @@
 package com.Atmatm6.schematichub;
 
+import com.Atmatm6.schematichub.config.ReferenceStrings;
 import com.Atmatm6.schematichub.schematic.CreateConfig;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.config.Configuration;
 
-@Mod(modid = "sgh", name = "Schematic Github Connector", version = "1.0-1.7.10")
+@Mod(modid = ReferenceStrings.modid, name = "Schematic Github Connector", version = "1.0-1.7.10")
 public class SchematicGithubConnector {
     public static void preInit(FMLPreInitializationEvent event)
     {
@@ -13,5 +15,9 @@ public class SchematicGithubConnector {
         Configuration config = new Configuration(event.getSuggestedConfigurationFile());
         config.load();
         config.save();
+    }
+    public static void init(FMLInitializationEvent event)
+    {
+
     }
 }
---
+++ src/main/java/com/Atmatm6/schematichub/config/ReferenceStrings.java
@@ -0,0 +1,8 @@
+package com.Atmatm6.schematichub.config;
+
+/**
+ * Created by Austin on 2015-01-17.
+ */
+public class ReferenceStrings {
+    public static final String modid = "SchematicGitConnect";
+}
